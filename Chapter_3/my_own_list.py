88acac6b2911f42dc50679cd3bcbf30841c0690b
ethangm0907 <ethangm0907@gmail.com>
2026-02-05 17:18:31 -0500

    Create my_own_list.py

---
+++ Chapter_3/my_own_list.py
@@ -0,0 +1,8 @@
+# List of cars I like
+cars = ["Honda Civic", "Toyota Corolla", "Tesla Model 3", "Ford Mustang"]
+
+# Print statements using the list
+print(f"I would like to own a {cars[0]}.")
+print(f"I would like to own a {cars[1]}.")
+print(f"I would like to own a {cars[2]}.")
+print(f"I would like to own a {cars[3]}.")
