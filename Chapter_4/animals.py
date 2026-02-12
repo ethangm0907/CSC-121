17a58c2457999e5d6da3cf8fb694fdb082a2bbf8
ethangm0907 <ethangm0907@gmail.com>
2026-02-12 16:02:48 -0500

    Create animals.py
    Add animals.py

---
+++ Chapter_4/animals.py
@@ -0,0 +1,26 @@
+# 4-2. Animals
+
+animals = ["dog", "cat", "rabbit"]
+
+# Print each animal name
+for animal in animals:
+    print(animal)
+
+print()  # blank line to make the output easier to read
+
+# Print a statement about each animal
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+# What they have in common
+print("\nAny of these animals would make a great pet!")
+
+dog
+cat
+rabbit
+
+A dog would make a great pet.
+A cat would make a great pet.
+A rabbit would make a great pet.
+
+Any of these animals would make a great pet!
