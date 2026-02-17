3ad7b960f24c3e861c83db423bd2258ec8137f34
ethangm0907 <ethangm0907@gmail.com>
2026-02-17 12:18:57 -0800

    Create alien_colors.py

---
+++ Chapter_5/alien_colors.py
@@ -0,0 +1,10 @@
+# 5-3. Alien Colors (Final Version: if-elif-else)
+
+alien_color = "green"   # Change this to "yellow" or "red" to test other versions
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
